f03bf92c910faf2e01987426705d5bb5859671eb
EricksonLopes <ofc.erickson@gmail.com>
2020-07-09 11:43:22 -0300

    implementing login verification

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.http import HttpResponse
+
 
 def login(request):
-    return render(request, 'my_app/login.html')
+    if request.method == 'GET':
+        return render(request, 'my_app/login.html')
+
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+
+    user = authenticate(username=username, password=password)
+
+    # se for True
+    if user:
+        return HttpResponse('Credenciais validas')
+    return HttpResponse('Credenciais inválidas')
