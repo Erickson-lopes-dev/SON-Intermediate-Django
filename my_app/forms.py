0314091d410c607de3ff4a7c5081befe3b9917c2
EricksonLopes <ofc.erickson@gmail.com>
2020-07-10 11:23:25 -0300

    stylizing forms with django modeling

--- my_app/forms.py
+++ my_app/forms.py
@@ -3,9 +3,21 @@ from .models import STATES_CHOICES
 
 
 class AddressForm(forms.Form):
-    address = forms.CharField(max_length=255)
+    address = forms.CharField(max_length=255,
+                              widget=forms.TextInput(attrs={'class': 'form-control'})
+                              )
     # campo não requerido
-    address_complement = forms.CharField(max_length=255, required=False)
-    city = forms.CharField(max_length=255)
-    state = forms.ChoiceField(choices=STATES_CHOICES)
-    contry = forms.CharField(max_length=255)
+    address_complement = forms.CharField(max_length=255,
+                                         widget=forms.TextInput(attrs={'class': 'form-control'}),
+                                         required=False
+                                         )
+    city = forms.CharField(max_length=255,
+                           widget=forms.TextInput(attrs={'class': 'form-control'})
+                           )
+    state = forms.ChoiceField(choices=STATES_CHOICES,
+                              widget=forms.Select(attrs={'class': 'form-control'})
+                              )
+
+    contry = forms.CharField(max_length=255,
+                             widget=forms.TextInput(attrs={'class': 'form-control'})
+                             )
